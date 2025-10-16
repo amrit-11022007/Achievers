7dfe8218a0e389cb6794874ab7785c2c3c28b4fe
AMAN POKHARIA <pokhariaaman2@gmail.com>
2025-10-16 21:00:32 +0530

    Update script.js

--- complain/script.js
+++ complain/script.js
@@ -1,13 +1,1 @@
- function checkLogin() {
-      const loggedIn = localStorage.getItem("userLoggedIn");
-      const username = localStorage.getItem("Email");
-      const Pass = localStorage.getItem("Password");
 
-      if (!loggedIn) {
-        // Not logged in, redirect to login.html with redirect back to complain
-        window.location.href = "login.html?redirect=complain.html";
-      } else {
-        // Show username if logged in
-        document.getElementById("welcome").innerText = "Welcome, " + username + "!";
-      }
-    }
